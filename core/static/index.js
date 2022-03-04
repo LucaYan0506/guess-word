05855e09d4435ce803ef2124e69143c7a41c89b7
Luca Yan <lucayan0506>
2022-03-04 20:07:04 +0000

    first commit

---
+++ core/static/index.js
@@ -0,0 +1,30 @@
+setInterval(() => {
+    let timer = document.querySelector('#timer');
+    
+    let sec = parseInt(timer.innerHTML.substr(10,2)) - 1;
+    let min = parseInt(timer.innerHTML.substr(4,2));
+    let hours = parseInt(timer.innerHTML.substr(0,2));
+
+
+    if (sec <= 0){
+        sec = 59;
+        min--;
+    }
+    if (min < 0){
+        min = 59;
+        hours--;
+    }
+
+    if (hours < 0){
+        location.reload();
+    } 
+
+    if (sec < 10)
+        sec = "0" + sec;
+    if (min < 10)
+        min = "0" + min;
+    if (hours < 10)
+        hours = "0" + hours;
+
+    timer.innerHTML = hours + "h " + min + "min " + sec + "sec";
+}, 1000);
