eae30b23d594329e39dcc291f9ba1bb13d35df2c
Luca Yan <lucayan0506>
2022-03-13 12:42:44 +0000

    allow heroku host

--- Guess_word/settings.py
+++ Guess_word/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-pnx^x*ec$mx)3ui!sc#co*&m)ybu2)h+mcrd2pc01xi_t383sn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['guess-word1.herokuapp.com']
 
 
 # Application definition
