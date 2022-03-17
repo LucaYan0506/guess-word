b4212604510ab447708cdcaa573622b552b858c8
Luca Yan <lucayan0506>
2022-03-17 19:49:30 +0000

    update task to allow Redis to update database

--- core/tasks.py
+++ core/tasks.py
@@ -3,22 +3,24 @@ from celery import shared_task
 from Guess_word.celery import app
 from .models import Words
 import random,datetime,os
+from django.db import transaction
 
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
         CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 @shared_task
 def get_random_word():
-    random_n = random.randint(1, 32246)
+    with transaction.atomic():
+        random_n = random.randint(1, 32246)
 
-    f = open("word_list.txt", "r")
-    for x in range(random_n):
-        f.readline()
-    today_word = f.readline().replace("\n","")
-    f.close()
-    words = Words(word=today_word, date=datetime.datetime.now())
-    words.save()
-    return today_word
+        f = open("word_list.txt", "r")
+        for x in range(random_n):
+            f.readline()
+        today_word = f.readline().replace("\n","")
+        f.close()
+        words = Words(word=today_word, date=datetime.datetime.now())
+        words.save()
+        return today_word
 
 @shared_task
 def test1(test):
