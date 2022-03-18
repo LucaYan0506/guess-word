b641ba797ddba3812937318ff9d532b5e79796a4
Luca Yan <lucayan0506>
2022-03-18 17:01:15 +0000

    restore periodic task to default setting (which means that it will run once per day at 23:59)

--- Guess_word/settings.py
+++ Guess_word/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BEAT_SCHEDULE = {
     'scheduled_task':{
         "task": "core.tasks.get_random_word",
-        'schedule':20.0,
+        'schedule':crontab(minute=59, hour="23"),
         'args':(),
     }
 }
