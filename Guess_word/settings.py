3532f6b66a58d70b09ce4faeb1bcde6c91557789
Luca Yan <lucayan0506>
2022-03-21 18:19:50 +0000

    try to use celery

--- Guess_word/settings.py
+++ Guess_word/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-pnx^x*ec$mx)3ui!sc#co*&m)ybu2)h+mcrd2pc01xi_t383sn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://guess-word1.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['guess-word1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BEAT_SCHEDULE = {
     'scheduled_task':{
         "task": "core.tasks.get_random_word",
-        'schedule':crontab(minute=19, hour=18),
+        'schedule':crontab(minute=21, hour=18),
         'args':(),
     }
 }
