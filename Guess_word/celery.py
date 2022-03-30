a01dbb525aa54116495ad80a1593d77645c62858
Luca Yan <lucayan0506>
2022-03-30 20:41:52 +0100

    update/fix website, now today words should update

--- Guess_word/celery.py
+++ Guess_word/celery.py
@@ -11,9 +11,12 @@ app.config_from_object('django.conf:settings', namespace= 'CELERY')
 app.autodiscover_tasks()
 
 ## Get the base REDIS URL, default to redis' default
+BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.broker_url = BASE_REDIS_URL
--- core/tasks.py
+++ core/tasks.py
@@ -3,19 +3,20 @@ from celery import shared_task
 from Guess_word.celery import app
 import random,os
 import requests
-
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+        CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 @shared_task
 def get_random_word():
-        random_n = random.randint(1, 5756)
+        random_n = random.randint(1, 32246)
 
-        f = open("guess_word_list.txt", "r")
+        f = open("word_list.txt", "r")
         for x in range(random_n):
             f.readline()
         today_word = f.readline().replace("\n","")
         f.close()
       
-        url = 'http://127.0.0.1:8000/ksdjlfasadfasjfklha/'
+        url = 'https://guess-word1.herokuapp.com/ksdjlfasadfasjfklha/'
         
         myobj = {
             'token':'jqw"£fds}dsfefhqwjehf23kasdfa!£*(£*$^"whfja"*JFKEAHr439',
