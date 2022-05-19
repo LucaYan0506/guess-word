3c3bb9f8f3784e4c199d0f822dfd2d4cb6df8beb
Luca Yan <lucayan0506>
2022-05-19 22:55:28 +0100

    add sitemap + fix bug

--- Guess_word/settings.py
+++ Guess_word/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'django_celery_beat'
+    'django_celery_beat',
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
